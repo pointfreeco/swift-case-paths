e8b6a10350fa01df000b4024c2748fade6cd832d
Corey McMahon <corey.d.mcmahon@gmail.com>
2020-02-04 18:51:19 -0600

    add docstring for subscript. i am bad at writing these

--- Sources/CasePaths/CasePathSubscriptable.swift
+++ Sources/CasePaths/CasePathSubscriptable.swift
@@ -2,6 +2,10 @@ public protocol CasePathSubscriptable {}
 
 /// Opt in protocol to allow subscripting of case paths in a similar way to key paths on structs
 public extension CasePathSubscriptable {
+  /// Attempts to extract a value from an enum
+  ///
+  /// - Parameter casePath: a case path for this enum
+  /// - Returns: A value iff it can be extracted from the given root, otherwise `nil`.
   subscript <T>(casePath casePath: CasePath<Self, T>) -> T? {
     get {
       return casePath.extract(from: self)
