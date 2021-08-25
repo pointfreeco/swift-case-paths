1a00fb6b5e766dc3c30f34a7a4dbfa256aa4ab8c
Maximilian Rogers <maxroger@amazon.com>
2021-08-25 00:11:34 -0400

    adding explicit platform support

--- Package.swift
+++ Package.swift
@@ -3,30 +3,36 @@
 import PackageDescription
 
 let package = Package(
-  name: "swift-case-paths",
-  products: [
-    .library(
-      name: "CasePaths",
-      targets: ["CasePaths"]
-    )
-  ],
-  dependencies: [
-    .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0")
-  ],
-  targets: [
-    .target(
-      name: "CasePaths"
-    ),
-    .testTarget(
-      name: "CasePathsTests",
-      dependencies: ["CasePaths"]
-    ),
-    .target(
-      name: "swift-case-paths-benchmark",
-      dependencies: [
-        "CasePaths",
-        .product(name: "Benchmark", package: "Benchmark"),
-      ]
-    ),
-  ]
+    name: "swift-case-paths",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
+    products: [
+        .library(
+            name: "CasePaths",
+            targets: ["CasePaths"]
+        )
+    ],
+    dependencies: [
+        .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0")
+    ],
+    targets: [
+        .target(
+            name: "CasePaths"
+        ),
+        .testTarget(
+            name: "CasePathsTests",
+            dependencies: ["CasePaths"]
+        ),
+        .target(
+            name: "swift-case-paths-benchmark",
+            dependencies: [
+                "CasePaths",
+                .product(name: "Benchmark", package: "Benchmark"),
+            ]
+        ),
+    ]
 )
