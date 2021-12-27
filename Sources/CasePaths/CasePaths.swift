c7fe87d237efc2551503291ce5718554affa4a15
maximkrouk <id.maximkrouk@gmail.com>
2021-12-27 18:58:01 +0300

    feat: ifCaseLetEmbed

--- Sources/CasePaths/CasePaths.swift
+++ Sources/CasePaths/CasePaths.swift
@@ -54,3 +54,29 @@ extension CasePath where Value: LosslessStringConvertible, Root == String {
     )
   }
 }
+
+extension CasePath {
+  /// Helper function for enum properies
+  ///
+  /// Usage
+  /// ```
+  /// enum Enum {
+  ///   case a(Int)
+  ///   case b(Bool)
+  ///
+  ///   var a: Int? {
+  ///     get { (/Self.a).extract(from: self) }
+  ///     set { (/Self.a).ifCaseLetEmbed(newValue, in: &self) }
+  ///   }
+  ///
+  ///   var b: Bool? {
+  ///     get { (/Self.b).extract(from: self) }
+  ///     set { (/Self.b).ifCaseLetEmbed(newValue, in: &self) }
+  ///   }
+  /// }
+  /// ```
+  public func ifCaseLetEmbed(_ value: Value?, in root: inout Root) {
+    guard extract(from: root) != nil, let value = value else { return }
+    root = embed(value)
+  }
+}
