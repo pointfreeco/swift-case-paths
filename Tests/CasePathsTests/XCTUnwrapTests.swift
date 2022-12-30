0bc91113ea279ee2b7eb03f72e70f0ca33ba182c
Brandon Williams <mbrandonw@hey.com>
2022-12-30 12:06:55 -0800

    wip

--- Tests/CasePathsTests/XCTUnwrapTests.swift
+++ Tests/CasePathsTests/XCTUnwrapTests.swift
@@ -1,30 +1,31 @@
-import CasePaths
-import XCTest
+#if DEBUG
+  import CasePaths
+  import XCTest
 
-final class XCTUnwrapTests: XCTestCase {
-  func testXCTUnwrapFailure() throws {
-    XCTExpectFailure {
-      $0.compactDescription == """
-        XCTUnwrap failed: expected non-nil value of type "Error"
-        """
+  final class XCTUnwrapTests: XCTestCase {
+    func testXCTUnwrapFailure() throws {
+      XCTExpectFailure {
+        $0.compactDescription == """
+          XCTUnwrap failed: expected non-nil value of type "Error"
+          """
+      }
+      _ = try XCTUnwrap(Result<Int, Error>.success(2), case: /Result.failure)
     }
-    _ = try XCTUnwrap(Result<Int, Error>.success(2), case: /Result.failure)
-  }
 
-  func testXCTUnwrapFailure_WithMessage() throws {
-    XCTExpectFailure {
-      $0.compactDescription == """
-        XCTUnwrap failed: expected non-nil value of type "Error" - Should be success
-        """
+    func testXCTUnwrapFailure_WithMessage() throws {
+      XCTExpectFailure {
+        $0.compactDescription == """
+          XCTUnwrap failed: expected non-nil value of type "Error" - Should be success
+          """
+      }
+      _ = try XCTUnwrap(Result<Int, Error>.success(2), case: /Result.failure, "Should be success")
     }
-    _ = try XCTUnwrap(Result<Int, Error>.success(2), case: /Result.failure, "Should be success")
-  }
 
-  func testXCTUnwrapPass() throws {
-    XCTAssertEqual(
-      try XCTUnwrap(Result<Int, Error>.success(2), case: /Result.success),
-      2
-    )
+    func testXCTUnwrapPass() throws {
+      XCTAssertEqual(
+        try XCTUnwrap(Result<Int, Error>.success(2), case: /Result.success),
+        2
+      )
+    }
   }
-}
-
+#endif
