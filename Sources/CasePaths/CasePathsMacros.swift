78689ebce390f658c4dffb2c8dedd44c7eee64af
Stephen Celis <stephen@stephencelis.com>
2023-06-07 19:52:05 -0700

    wip

--- Sources/CasePaths/CasePathsMacros.swift
+++ Sources/CasePaths/CasePathsMacros.swift
@@ -2,6 +2,7 @@
 @attached(member, names: arbitrary)
 public macro CasePathable() = #externalMacro(module: "CasePathsMacros", type: "CasePathableMacro")
 
+// NB: `#case` is not allowed: https://github.com/apple/swift/issues/66444
 @freestanding(expression)
 public macro casePath<Root: CasePathable, Value>(
   _ keyPath: KeyPath<Root, Value?>
