4667b0494cfdc8f234bd59f54c398b5daa39912b
Brandon Williams <mbrandonw@hey.com>
2023-10-15 19:11:40 -0400

    tests for modify

--- Sources/CasePaths/CasePathable.swift
+++ Sources/CasePaths/CasePathable.swift
@@ -421,7 +421,7 @@ extension CasePathable {
   ) {
     let `case` = Case(keyPath)
     guard var value = `case`.extract(self) else {
-      XCTFail("Can't modify \(self) with case key path \(keyPath)", file: file, line: line)
+      XCTFail("Can't modify '\(self)' at path \(keyPath)", file: file, line: line)
       return
     }
     yield(&value)
---
+++ Tests/CasePathsTests/CasePathableTests.swift
@@ -0,0 +1,28 @@
+import CasePaths
+import XCTest
+
+final class CasePathableTests: XCTestCase {
+  func testModify() {
+    var response = Response.success(1)
+    response.modify(\.success) { $0 += 1 }
+    XCTAssertEqual(response, .success(2))
+  }
+
+  func testModifyWrongCase() {
+    var response = Response.failure
+    XCTExpectFailure {
+      response.modify(\.success) { $0 += 1 }
+    } issueMatcher: {
+      $0.compactDescription == #"""
+        Can't modify 'failure' at path \Case<Response>.subscript(dynamicMember: <unknown>)
+        """#
+    }
+    XCTAssertEqual(response, .failure)
+  }
+}
+
+@CasePathable
+private enum Response: Equatable {
+  case success(Int)
+  case failure
+}
