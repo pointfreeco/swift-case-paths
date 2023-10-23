a217a500704c6ce3469c1e5cf7ff6ca1157f5d47
Stephen Celis <stephen@stephencelis.com>
2023-10-22 19:01:04 -0700

    wip

--- Sources/CasePaths/Never+CasePathable.swift
+++ Sources/CasePaths/Never+CasePathable.swift
@@ -1,13 +1,5 @@
-extension Never: CasePathable, CasePathIterable {
-  public struct AllCasePaths: RandomAccessCollection {
-    public var startIndex: Int { 0 }
-    public var endIndex: Int { 0 }
-    public func index(after i: Int) -> Int { i + 1 }
-    public func index(before i: Int) -> Int { i - 1 }
-    public subscript(position: Int) -> PartialCaseKeyPath<Never> {
-      fatalError("Index out of range")
-    }
-  }
+extension Never: CasePathable {
+  public struct AllCasePaths {}
   public static var allCasePaths: AllCasePaths {
     AllCasePaths()
   }
