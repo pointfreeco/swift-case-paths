a8f9e09f71cea9ef70daf473c91cf997bf7fb9b8
Stephen Celis <stephen@stephencelis.com>
2023-10-24 11:26:20 -0700

    wip

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,5 +1,6 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
@@ -17,13 +18,16 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
+    .package(url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
+    .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
   ],
   targets: [
     .target(
       name: "CasePaths",
       dependencies: [
+        "CasePathsMacros",
         .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay")
       ]
     ),
@@ -31,11 +35,25 @@ let package = Package(
       name: "CasePathsTests",
       dependencies: ["CasePaths"]
     ),
+    .macro(
+      name: "CasePathsMacros",
+      dependencies: [
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+      ]
+    ),
+    .testTarget(
+      name: "CasePathsMacrosTests",
+      dependencies: [
+        "CasePathsMacros",
+        .product(name: "MacroTesting", package: "swift-macro-testing"),
+      ]
+    ),
     .executableTarget(
       name: "swift-case-paths-benchmark",
       dependencies: [
         "CasePaths",
-        .product(name: "Benchmark", package: "Benchmark"),
+        .product(name: "Benchmark", package: "swift-benchmark"),
       ]
     ),
   ]
