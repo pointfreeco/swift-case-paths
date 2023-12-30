b763dc4e2ed1490f3dab6480c509eb231fb239c0
Stephen Celis <stephen@stephencelis.com>
2023-12-30 13:19:21 -0800

    wip

--- Sources/swift-case-paths-benchmark/main.swift
+++ Sources/swift-case-paths-benchmark/main.swift
@@ -12,8 +12,8 @@ import CasePaths
  Case Key Path (Appended: 2): Extract                       4541.000 ns ±   6.09 %     307882
  Case Key Path (Appended: 2, Cached): Embed                 1958.000 ns ±  17.72 %     709441
  Case Key Path (Appended: 2, Cached): Extract               2417.000 ns ± 118.76 %     572773
- Case Key Path (Appended: 2, Cached + Converted): Embed      958.000 ns ±  14.47 %    1000000
- Case Key Path (Appended: 2, Cached + Converted): Extract   1417.000 ns ±  15.08 %     990935
+ Case Key Path (Appended: 2, Cached + Converted): Embed       83.000 ns ±  26.97 %    1000000
+ Case Key Path (Appended: 2, Cached + Converted): Extract    208.000 ns ±  17.14 %    1000000
  Case Path Reflection (Appended: 10): Embed                 1709.000 ns ±   8.49 %     816699
  Case Path Reflection (Appended: 10): Extract               4750.000 ns ±   6.49 %     293788
  Case Path Reflection (Appended: 10, Cached): Embed           83.000 ns ±  28.79 %    1000000
@@ -22,9 +22,9 @@ import CasePaths
  Case Key Path (Appended: 10): Extract                     13833.000 ns ± 167.15 %     102161
  Case Key Path (Appended: 10, Cached): Embed                8000.000 ns ±   4.86 %     175509
  Case Key Path (Appended: 10, Cached): Extract              9333.000 ns ±   5.80 %     150034
- Case Key Path (Appended: 10, Cached + Converted): Embed    3625.000 ns ±   5.56 %     381696
- Case Key Path (Appended: 10, Cached + Converted): Extract  4875.000 ns ±   8.08 %     285089
- Case Pathable (Dynamic Member Lookup: 10)                     0.000 ns ±    inf %     1000000
+ Case Key Path (Appended: 10, Cached + Converted): Embed      83.000 ns ±  64.15 %    1000000
+ Case Key Path (Appended: 10, Cached + Converted): Extract   791.000 ns ±  33.26 %    1000000
+ Case Pathable (Dynamic Member Lookup: 10)                     0.000 ns ±    inf %    1000000
  */
 
 benchmark("Case Path Reflection (Appended: 2): Embed") {
@@ -61,7 +61,7 @@ benchmark("Case Key Path (Appended: 2, Cached): Extract") {
   doNotOptimizeAway(Result<Int?, any Error>.success(.some(42))[case: ckp])
 }
 
-let acp = AnyCasePath(ckp)
+let acp = ckp.asCasePath()
 benchmark("Case Key Path (Appended: 2, Cached + Converted): Embed") {
   doNotOptimizeAway(acp.embed(42))
 }
@@ -136,7 +136,7 @@ benchmark("Case Key Path (Appended: 2, Cached + Converted): Extract") {
     doNotOptimizeAway(Foo.foo(.foo(.foo(.foo(.foo(.foo(.foo(.foo(.foo(.bar)))))))))[case: ckp2])
   }
 
-  let acp2 = AnyCasePath(ckp2)
+  let acp2 = ckp2.asCasePath()
   benchmark("Case Key Path (Appended: 10, Cached + Converted): Embed") {
     doNotOptimizeAway(acp2.embed(()))
   }
