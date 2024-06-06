ecb5b29e65bfc58b4c90216d4ca6ef3a573a28a7
stephencelis <stephencelis@users.noreply.github.com>
2024-06-06 22:09:17 +0000

    Run swift-format

--- Tests/CasePathsTests/MacroTests.swift
+++ Tests/CasePathsTests/MacroTests.swift
@@ -1,12 +1,12 @@
 #if swift(>=5.9)
   import CasePaths
-  
+
   @CasePathable
   private enum Comments {
     // Comment above case
     case bar
     /*Comment before case*/ case baz(Int)
     case fizz(buzz: String)  // Comment on case
-    case fizzier/*Comment in case*/(Int, buzzier: String)
+    case fizzier /*Comment in case*/(Int, buzzier: String)
   }
 #endif
