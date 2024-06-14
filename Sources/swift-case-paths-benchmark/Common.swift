f0e40401407ad75407d6de70e5d087b284c23874
Stephen Celis <stephen@stephencelis.com>
2024-06-14 10:30:43 -0700

    try removing benchmarks

--- Sources/swift-case-paths-benchmark/Common.swift
+++
@@ -1,7 +0,0 @@
-@inline(__always)
-func doNotOptimizeAway<T>(_ x: T) {
-  @_optimize(none)
-  func assumePointeeIsRead(_ x: UnsafeRawPointer) {}
-
-  withUnsafePointer(to: x) { assumePointeeIsRead($0) }
-}
