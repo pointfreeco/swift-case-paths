24de5a3d103bda453a1d56ae0aaa8f999dac9d61
Stephen Celis <stephen@stephencelis.com>
2024-10-23 12:26:51 -0700

    Update KeyPath+Sendable.swift

--- Sources/CasePaths/Internal/KeyPath+Sendable.swift
+++ Sources/CasePaths/Internal/KeyPath+Sendable.swift
@@ -1,10 +1,7 @@
 #if compiler(>=6)
   public typealias _SendableKeyPath<Root, Value> = any KeyPath<Root, Value> & Sendable
-  public typealias _SendableWritableKeyPath<Root, Value> = any WritableKeyPath<Root, Value>
-    & Sendable
 #else
   public typealias _SendableKeyPath<Root, Value> = KeyPath<Root, Value>
-  public typealias _SendableWritableKeyPath<Root, Value> = WritableKeyPath<Root, Value>
 #endif
 
 // NB: Dynamic member lookup does not currently support sendable key paths and even breaks
@@ -18,16 +15,6 @@ extension _AppendKeyPath {
   where Self == KeyPath<Root, Value> {
     #if compiler(>=6)
       unsafeBitCast(self, to: _SendableKeyPath<Root, Value>.self)
-    #else
-      self
-    #endif
-  }
-
-  @_transparent
-  package func unsafeSendable<Root, Value>() -> _SendableWritableKeyPath<Root, Value>
-  where Self == WritableKeyPath<Root, Value> {
-    #if compiler(>=6)
-      unsafeBitCast(self, to: _SendableWritableKeyPath<Root, Value>.self)
     #else
       self
     #endif
