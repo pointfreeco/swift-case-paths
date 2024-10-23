4b109cf8140ecca0174d26a93957e8edf7fafd51
Stephen Celis <stephen@stephencelis.com>
2024-10-23 12:55:25 -0700

    fix

--- Sources/CasePaths/Internal/KeyPath+Sendable.swift
+++ Sources/CasePaths/Internal/KeyPath+Sendable.swift
@@ -1,3 +1,9 @@
+#if compiler(>=6)
+  public typealias _SendableKeyPath<Root, Value> = any Sendable & KeyPath<Root, Value>
+#else
+  public typealias _SendableKeyPath<Root, Value> = KeyPath<Root, Value>
+#endif
+
 // NB: Dynamic member lookup does not currently support sendable key paths and even breaks
 //     autocomplete.
 //
@@ -5,10 +11,10 @@
 //     * https://github.com/swiftlang/swift/issues/77105
 extension _AppendKeyPath {
   @_transparent
-  package func unsafeSendable<Root, Value>() -> any Sendable & KeyPath<Root, Value>
+  package func unsafeSendable<Root, Value>() -> _SendableKeyPath<Root, Value>
   where Self == KeyPath<Root, Value> {
     #if compiler(>=6)
-      unsafeBitCast(self, to: (any Sendable & KeyPath<Root, Value>).self)
+      unsafeBitCast(self, to: _SendableKeyPath<Root, Value>.self)
     #else
       self
     #endif
