3412f83c15c6be11e34465e68dfb4b3fa85002c2
Stephen Celis <stephen@stephencelis.com>
2025-09-17 11:23:45 -0700

    Swift Syntax 602 Support

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"602.0.0"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"603.0.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
   ],
   targets: [
