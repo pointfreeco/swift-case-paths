bc2350be775e1b5275c4165c3597a70d110244e8
Stephen Celis <stephen@stephencelis.com>
2025-11-10 21:58:20 -0800

    wip

--- Sources/CasePathsMacros/Internal/Nonisolated.swift
+++ Sources/CasePathsMacros/Internal/Nonisolated.swift
@@ -1,4 +1,8 @@
-import SwiftSyntax
+#if canImport(SwiftSyntax600)
+  import SwiftSyntax
+#else
+  @preconcurrency import SwiftSyntax
+#endif
 
 #if compiler(>=6.1)
   let nonisolated: TokenSyntax? = .keyword(.nonisolated, trailingTrivia: .space)
