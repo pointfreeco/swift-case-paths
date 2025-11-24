ceb9e4a91a904e0cdaf752a982e5e42044a16f4f
Jairon Terrero <jairon@passivelogic.com>
2025-11-24 10:54:29 -0700

    fix: Include SwiftDiagnostics
    This product is used in `Sources/CasePathsMacros/CasePathableMacro.swift` but is not included in the dependencies for `CasePathsMacros`.

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -46,6 +46,7 @@ let package = Package(
       dependencies: [
         .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
         .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+        .product(name: "SwiftDiagnostics", package: "swift-syntax"),
       ]
     ),
     .testTarget(
