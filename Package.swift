6a156dbaed800bf9f53e7eef11bf59eb4448194a
Marc Schwieterman <marc@marcisme.com>
2026-06-09 19:14:58 -0700

    Remove warning treatment for Swift 6.4 and above

--- Package.swift
+++ Package.swift
@@ -80,9 +80,4 @@ for target in package.targets {
     .enableUpcomingFeature("MemberImportVisibility"),
     .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
   ])
-  #if compiler(>=6.4)
-    target.swiftSettings?.append(contentsOf: [
-      .treatAllWarnings(as: .error)
-    ])
-  #endif
 }
